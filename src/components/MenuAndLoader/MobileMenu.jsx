46ffb137711ab8a378f07b453b636ed738d767da
yevheniilim <pacmanlim27@gmail.com>
2021-01-11 15:51:31 +0200

    working progress

--- src/components/MenuAndLoader/MobileMenu.jsx
+++ src/components/MenuAndLoader/MobileMenu.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 import { openMenu } from "../../redux/header-reducer";
+import { openAdress, openSubMenu } from "../../redux/mobileMenu-reducer";
 import style from "./menuAndLoader.module.css";
 
 let MobileMenu = (props) => {
+    // debugger
   let showMenu = props.isMobileMenuOpen ? style.openMenu : style.closeMenu;
   let mobileClasess = style.mobileMenu + " " + showMenu;
+  let showSubMenu = props.isMobileSubMenuOpen ? style.subMenuOpen : style.subMenuClose;
+  let menuListClasess = style.menuList + " " + showSubMenu;
+  let rotateChevron = props.isMobileSubMenuOpen ? style.chevronRound : style.chevronUp;
+  let chevronClass = style.chevron + " " + rotateChevron;
+
+  let liSubMenuItems = props.subMenuItems.map(i => <li key={i.id}>{i.name}</li>)
+
+  let  showAdress = props.isAdressOpen ? style.subMenuOpen : style.subMenuClose
+  let adressListClasess = style.menuList + ' ' + showAdress;
+  let rotateAdress =  props.isAdressOpen ? style.chevronRound : style.chevronUp;
+  let adressChevron = style.chevron + " " + rotateAdress;
 
   return (
     <div className={mobileClasess}>
@@ -15,7 +28,60 @@ let MobileMenu = (props) => {
         </div>
         <button onClick={props.openMenu}> close</button>
       </div>
-      <p>HELLO</p>
+      <div className={style.allMenu}>
+        <div>Главная</div>
+        <div onClick={props.openSubMenu} className={style.menuItem}>
+          <div>
+            Меню
+            <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={chevronClass}
+              >
+                <polyline points="6 9 12 15 18 9"></polyline>
+              </svg>
+            </p>
+          </div>
+          <ul className={menuListClasess}>
+                {liSubMenuItems}
+          </ul>
+        </div>
+        <div>Корзина</div>
+        <div>Акции</div>
+        <div onClick={props.openAdress} className={style.menuItem}>
+           <div>Наши рестораны
+           <p>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="30"
+                height="30"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className={adressChevron}
+              >
+                <polyline points="6 9 12 15 18 9"></polyline>
+              </svg>
+            </p>
+           </div>
+           <ul className={adressListClasess}>
+            <li>Adress</li>
+            <li>Adress</li>
+           </ul>
+
+        </div>
+      </div>
     </div>
   );
 };
@@ -24,9 +90,14 @@ let mapStateToProps = (state) => {
   return {
     isMobileMenuOpen: state.headerReducer.isMobileMenuOpen,
     srcLogo: state.headerReducer.srcLogo,
+    isMobileSubMenuOpen: state.mobileMenu.isMobileSubMenuOpen,
+    subMenuItems: state.mobileMenu.subMenuItems,
+    isAdressOpen: state.mobileMenu.isAdressOpen
   };
 };
 
-let MobileMenuContainer = connect(mapStateToProps, { openMenu })(MobileMenu);
+let MobileMenuContainer = connect(mapStateToProps, { openMenu, openSubMenu, openAdress})(
+  MobileMenu
+);
 
 export default MobileMenuContainer;
--- src/redux/mobileMenu-reducer.js
+++ src/redux/mobileMenu-reducer.js
@@ -1,12 +1,45 @@
 
+const OPEN_SUB_MENU = "OPEN_SUB_MENU"
+const OPEN_ADRESS = 'OPEN_ADRESS'
 
 let initialState = {
-
+    isMobileSubMenuOpen: false,
+    isAdressOpen: false,
+    subMenuItems: [
+        {name: 'Суши', id:1},
+        {name: 'Ролы', id:2},
+        {name: 'Супы', id:3},
+        {name: 'Лапша/Рис', id:4},
+        {name: 'Салаты', id:5},
+        {name: 'Маки', id:6}
+    ]
 }
 
 let mobileMenuReducer = (state = initialState, action) => {
-    return state
+    switch (action.type) {
+        case OPEN_SUB_MENU: {
+            return {
+                ...state,
+                isMobileSubMenuOpen: !state.isMobileSubMenuOpen
+            }
+        }
+        case OPEN_ADRESS: {
+            // console.log(state)
+            return {
+                ...state,
+                isAdressOpen: !state.isAdressOpen
+            }
+        }
+        default: {
+            return {
+                ...state
+            }
+        }
+    }
 }
 
+
+export let openSubMenu = () => ({type:OPEN_SUB_MENU})
+export let openAdress = () => ({type: OPEN_ADRESS})
 
 export default mobileMenuReducer
